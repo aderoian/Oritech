7932886c824206e3c92138e71c80f12496377e35
rearth <davidus2010@gmail.com>
2024-12-13 13:03:32 +0100

    Begin adding explosions (aka nuclear hellfile)

--- common/src/main/java/rearth/oritech/init/BlockContent.java
+++ common/src/main/java/rearth/oritech/init/BlockContent.java
@@ -208,6 +208,7 @@ public class BlockContent implements ArchitecturyBlockRegistryContainer {
     public static final Block REACTOR_FUEL_PORT = new ReactorFuelPortBlock(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK));
     public static final Block REACTOR_ABSORBER_PORT = new ReactorAbsorberPortBlock(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK));
     public static final Block REACTOR_ENERGY_PORT = new ReactorEnergyPortBlock(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK));
+    public static final Block REACTOR_EXPLOSION = new NuclearExplosionBlock(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK));
     
     // cooling cell, early game re-fillable component
     
