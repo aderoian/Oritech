45d5e8576207d8a378c0b9dc165a2440a794c1c0
rearth <davidus2010@gmail.com>
2024-12-16 16:25:06 +0100

    Improve cooler animation with particles, add cold biome bonus

--- common/src/main/java/rearth/oritech/block/blocks/processing/CoolerBlock.java
+++ common/src/main/java/rearth/oritech/block/blocks/processing/CoolerBlock.java
@@ -2,9 +2,17 @@ package rearth.oritech.block.blocks.processing;
 
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.NotNull;
 import rearth.oritech.block.base.block.MultiblockMachine;
 import rearth.oritech.block.entity.processing.CoolerBlockEntity;
+
+import java.util.List;
 
 public class CoolerBlock extends MultiblockMachine implements BlockEntityProvider {
     
@@ -15,5 +23,16 @@ public class CoolerBlock extends MultiblockMachine implements BlockEntityProvide
     @Override
     public @NotNull Class<? extends BlockEntity> getBlockEntityType() {
         return CoolerBlockEntity.class;
+    }
+    
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        super.appendTooltip(stack, context, tooltip, options);
+        
+        var showExtra = Screen.hasControlDown();
+        
+        if (showExtra) {
+            tooltip.add(Text.translatable("tooltip.oritech.cooler_block").formatted(Formatting.GRAY));
+        }
     }
 }
