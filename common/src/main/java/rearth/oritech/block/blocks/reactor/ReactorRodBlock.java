338670bda956191c6079f140f8d00ae598e75191
rearth <davidus2010@gmail.com>
2024-12-17 10:15:17 +0100

    Add proper outlines to reactor rods

--- common/src/main/java/rearth/oritech/block/blocks/reactor/ReactorRodBlock.java
+++ common/src/main/java/rearth/oritech/block/blocks/reactor/ReactorRodBlock.java
@@ -2,14 +2,22 @@ package rearth.oritech.block.blocks.reactor;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import rearth.oritech.init.BlockContent;
 
 public class ReactorRodBlock extends BaseReactorBlock {
     
     private final int rodCount;
     private final int internalPulseCount;
+    
+    private static final VoxelShape SOLO_SHAPE = Block.createCuboidShape(5, 0, 5, 11, 16, 11);
+    private static final VoxelShape DUO_SHAPE = Block.createCuboidShape(2, 0, 2, 14, 16, 14);
+    private static final VoxelShape QUAD_SHAPE = Block.createCuboidShape(1, 0, 1, 15, 16, 15);
     
     public ReactorRodBlock(Settings settings, int rodCount, int internalPulseCount) {
         super(settings);
@@ -22,6 +30,16 @@ public class ReactorRodBlock extends BaseReactorBlock {
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
         builder.add(Properties.LIT);
+    }
+    
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return switch (rodCount) {
+            case 1 -> SOLO_SHAPE;
+            case 2 -> DUO_SHAPE;
+            case 4 -> QUAD_SHAPE;
+            default -> SOLO_SHAPE;
+        };
     }
     
     public int getRodCount() {
