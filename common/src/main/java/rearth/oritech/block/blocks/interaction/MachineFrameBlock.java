260a959ec15548f851dde2ed295bcc16851f1b85
rearth <davidus2010@gmail.com>
2024-12-26 16:58:43 +0100

    Add machine frame config note

--- common/src/main/java/rearth/oritech/block/blocks/interaction/MachineFrameBlock.java
+++ common/src/main/java/rearth/oritech/block/blocks/interaction/MachineFrameBlock.java
@@ -1,9 +1,14 @@
 package rearth.oritech.block.blocks.interaction;
 
 import net.minecraft.block.*;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
@@ -13,6 +18,7 @@ import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 import rearth.oritech.Oritech;
 
+import java.util.List;
 import java.util.Objects;
 
 public class MachineFrameBlock extends Block {
@@ -35,6 +41,12 @@ public class MachineFrameBlock extends Block {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(NORTH, EAST, SOUTH, WEST);
+    }
+    
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        super.appendTooltip(stack, context, tooltip, options);
+        tooltip.add(Text.translatable("tooltip.oritech.machine_frame").formatted(Formatting.GRAY, Formatting.ITALIC));
     }
     
     @Override
